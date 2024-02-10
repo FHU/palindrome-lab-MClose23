b613aa29bc0af0835c1da954f8fb6abf54da06a5
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-10 12:58:59 -0600

    1.0 commit
    pass pytest?

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    word = word.replace(" ", "")
+    if not word:
+        return False 
+    if word == word[::-1]:
+        return True
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    word = input()
+    print(palindrome)
